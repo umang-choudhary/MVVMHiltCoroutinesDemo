d81a525e88c37d1faaa7759bdf845ee2cb1264ed
Umang Choudhary <uchoudhary@reddyice.com>
2024-09-13 20:08:01 +0530

    Dependencies upgrade.

--- app/src/main/java/com/app/koltinpoc/utils/Constants.kt
+++ app/src/main/java/com/app/koltinpoc/utils/Constants.kt
@@ -4,6 +4,6 @@ object Constants {
 
     val API_KEY = "18619cc7589947da9ea6f2864fad126d" //GET YOUR API KEY BY SIGNING to https://newsapi.org
     val BASE_URL = "https://newsapi.org/"
-    val COUNTRY_CODE = "in"
+    val COUNTRY_CODE = "us"
 
 }
